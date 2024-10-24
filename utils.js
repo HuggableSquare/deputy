93af8069452f0a87e88630d4afc29e6ef35cb8d9
nick <huggablesquare@gmail.com>
2024-10-24 06:30:01 +0000

    change id generation

--- utils.js
+++ utils.js
@@ -3,7 +3,6 @@ import path from 'node:path';
 import { Open } from 'unzipper';
 import mime, { Mime } from 'mime';
 import poppler from 'poppler-simple';
-import { createHash } from 'node:crypto';
 import { readdir, stat } from 'node:fs/promises';
 
 const comicMime = new Mime({
@@ -37,12 +36,16 @@ class Entity {
   parent;
   path;
 
-  constructor(dirent) {
+  async init() {
+    const stats = await stat(this.path);
+    const { ino, birthtimeMs } = stats;
+    this.id = this.parent ? `I${ino}D${birthtimeMs}` : 'index';
+    return stats;
+  }
+
+  constructor(dirent, parent) {
     this.path = path.join(dirent.path, dirent.name);
-    this.id = createId(this.path);
-    if (this.id !== 'index') {
-      this.parent = createId(dirent.path);
-    }
+    this.parent = parent;
   }
 }
 
@@ -62,19 +65,33 @@ class Directory extends Entity {
     return file.getThumbnail();
   }
 
+  async #read() {
+    const ents = (await readdir(this.path, { withFileTypes: true }))
+      .filter((ent) => ent.isDirectory() || ent.name.endsWith('cbz') || ent.name.endsWith('pdf'));
+
+    const entities = await Promise.all(ents.map((ent) => {
+      const type = ent.isDirectory() ? Directory : File;
+      return new type(ent, this.id).init();
+    }));
+
+    return entities
+      .flat()
+      .sort((a, b) => {
+        if (a.isDirectory !== b.isDirectory) {
+          return b.isDirectory - a.isDirectory;
+        }
+        // sort volumes before singles
+        if (a.name.startsWith('Vol') && !b.name.startsWith('Vol')) {
+          return -1;
+        }
+        return a.path.localeCompare(b.path, undefined, { numeric: true });
+      });
+  }
+
   async init() {
-    const entities = await readFolder(this.path);
-    entities.sort((a, b) => {
-      if (a.isDirectory !== b.isDirectory) {
-        return a.isDirectory ? -1 : 1;
-      }
-      // sort volumes before singles
-      if (a.name.startsWith('Vol') && !b.name.startsWith('Vol')) {
-        return -1;
-      }
-      return a.path.localeCompare(b.path, undefined, { numeric: true });
-    });
-    this.numberOfChildren = entities.filter((entity) => !entity.isDirectory).length;
+    await super.init();
+    const entities = await this.#read();
+    this.numberOfChildren = entities.filter(({ isDirectory }) => !isDirectory).length;
     this.children = entities.filter(({ parent }) => parent === this.id);
     // if the folder has no children, don't bother putting it in the list
     if (this.numberOfChildren === 0) return [];
@@ -83,8 +100,8 @@ class Directory extends Entity {
     return [this, ...entities];
   }
 
-  constructor(dirent) {
-    super(dirent);
+  constructor(dirent, parent) {
+    super(dirent, parent);
     this.name = dirent.name;
   }
 }
@@ -127,7 +144,7 @@ class File extends Entity {
   }
 
   async init() {
-    const { size } = await stat(this.path);
+    const { size } = await super.init();
     this.size = size;
 
     const images = await this.getImages();
@@ -137,32 +154,14 @@ class File extends Entity {
     return this;
   }
 
-  constructor(dirent) {
-    super(dirent);
+  constructor(dirent, parent) {
+    super(dirent, parent);
     this.name = fileNameFormat(dirent);
     this.fileType = comicMime.getType(this.path);
   }
 }
 
-function createId(string) {
-  if (string === dir) return 'index';
-  const hash = createHash('sha256');
-  return hash.update(string).digest('hex');
-}
-
 function fileNameFormat(ent) {
   const { name } = path.parse(ent.name);
   return name.replace(path.basename(ent.path), '').trim();
-}
-
-async function readFolder(dir) {
-  const ents = (await readdir(dir, { withFileTypes: true }))
-    .filter((ent) => ent.isDirectory() || ent.name.endsWith('cbz') || ent.name.endsWith('pdf'));
-
-  const entities = await Promise.all(ents.map((ent) => {
-    const type = ent.isDirectory() ? Directory : File;
-    return new type(ent).init();
-  }));
-
-  return entities.flat();
 }
